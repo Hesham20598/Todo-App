2d6ae39959b4bb9c3c0b321c97d57ad06eb89703
Hesham20598 <heshamghanem205@gmail.com>
2024-02-07 00:29:01 +0200

    finished splash activity

---
+++ app/src/main/java/UI/SplashActivity.kt
@@ -0,0 +1,34 @@
+package UI
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import com.example.todoapp.R
+
+@SuppressLint("CustomSplashScreen")
+@Suppress("DEPRECATION")
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        changeStatusBarColor(R.color.bg_color)
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+        Handler(mainLooper).postDelayed({
+            navigateToHomeActivity()
+        }, 2000)
+
+    }
+
+    private fun navigateToHomeActivity() {
+        startActivity(Intent(this, HomeActivity::class.java))
+        finish()
+    }
+
+    private fun changeStatusBarColor(colorResId: Int) {
+        window.statusBarColor = ContextCompat.getColor(this, colorResId)
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
+    }
+}
